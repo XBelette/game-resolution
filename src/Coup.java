71cb456730094cbeadc74ba966d2299bd034c540
XBelette <antonin.angibault@polytechnique.edu>
2015-05-08 19:09:58 +0200

    Coups are now comparable, and all equal.

--- src/Coup.java
+++ src/Coup.java
@@ -1,5 +1,5 @@
 
-public class Coup {
+public class Coup implements Comparable<Coup> {
 	public byte colonne;
 	public byte line;
 	
@@ -11,5 +11,13 @@ public class Coup {
 	public Coup(byte col, byte l){
 		colonne = col;
 		line = l;
+	}
+	
+	@Override
+	public int compareTo(Coup arg0) {
+		// There WILL be an heuristic for this later on
+		// There will have some problem for making it dependent on the game, but time for that later.
+		// For now, let's just say they are all equal.
+		return 0;
 	}
 }
