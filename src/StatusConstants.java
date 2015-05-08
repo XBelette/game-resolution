1e20f1e037b50c35019b76ad1c5bfe21dde01dcb
XBelette <antonin.angibault@polytechnique.edu>
2015-05-08 11:19:05 +0200

    added constants for game status (win, lose, draw)

---
+++ src/StatusConstants.java
@@ -0,0 +1,7 @@
+
+public class StatusConstants {
+	// Pretty much empty class containing constants for game statii.
+	final static byte WIN = 1;
+	final static byte DRAW = 0;
+	final static byte LOSE = -1;
+}
